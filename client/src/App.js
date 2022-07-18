9aeb073927c605f210c8f6d6fa26041820e4d9ce
zradev <zlatkoradevv@gmail.com>
2022-07-18 18:23:28 +0300

    Migrate to React.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Pages from './components/pages/Pages';
+
+function App() {
+  return (
+    <div className="App">
+      <Pages/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/pages/Pages.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+const Pages = () => {
+  return (
+    <>
+        <Router>
+            {/* Header */}
+            <Routes>
+                {/* Route */}
+            </Routes>
+            {/* Footer */}
+        </Router>
+    </>
+  )
+}
+
+export default Pages
