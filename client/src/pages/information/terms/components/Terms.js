b059f0273ce8463cad51b09ba1a35885cae99679
zradev <zlatkoradevv@gmail.com>
2022-08-16 15:50:12 +0300

    Add Terms Page

--- client/src/pages/information/terms/components/Terms.js
+++ client/src/pages/information/terms/components/Terms.js
@@ -4,7 +4,60 @@ import Heading from "../../../../components/common/Heading";
 const Terms = () => {
   return (
     <>
-      <Heading title="Terms And Conditions" />
+      <div className="container">
+        <Heading title="Terms And Conditions" />
+        <div className="content">
+          <h3>CREDIT CARDS</h3>
+          <p>
+            Visa, Master Card, American Express. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Ratione, alias deserunt! A
+            accusantium, earum neque itaque sequi incidunt? Esse ullam ad natus
+            culpa incidunt expedita reiciendis id ex quisquam blanditiis,
+            voluptatibus, laudantium a neque eius eligendi qui aut, ipsum
+            dolor..
+          </p>
+          <h3>BANK WIRE TRANSFER</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+            alias deserunt! A accusantium, earum neque itaque sequi incidunt?
+            Esse ullam ad natus culpa incidunt expedita reiciendis id ex
+            quisquam blanditiis, voluptatibus, laudantium a neque eius eligendi
+            qui aut, ipsum dolor..
+          </p>
+          <h3>DUTIES AND TAXES</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+            alias deserunt! A accusantium, earum neque itaque sequi incidunt?
+            Esse ullam ad natus culpa incidunt expedita reiciendis id ex
+            quisquam blanditiis, voluptatibus, laudantium a neque eius eligendi
+            qui aut, ipsum dolor..
+          </p>
+          <h3>EU VAT FREE</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+            alias deserunt! A accusantium, earum neque itaque sequi incidunt?
+            Esse ullam ad natus culpa incidunt expedita reiciendis id ex
+            quisquam blanditiis, voluptatibus, laudantium a neque eius eligendi
+            qui aut, ipsum dolor..
+          </p>
+          <h3>RETURN POLICY</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+            alias deserunt! A accusantium, earum neque itaque sequi incidunt?
+            Esse ullam ad natus culpa incidunt expedita reiciendis id ex
+            quisquam blanditiis, voluptatibus, laudantium a neque eius eligendi
+            qui aut, ipsum dolor..
+          </p>
+          <h3>HELP AND E-MAIL</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+            alias deserunt! A accusantium, earum neque itaque sequi incidunt?
+            Esse ullam ad natus culpa incidunt expedita reiciendis id ex
+            quisquam blanditiis, voluptatibus, laudantium a neque eius eligendi
+            qui aut, ipsum dolor..
+          </p>
+        </div>
+      </div>
     </>
   );
 };
